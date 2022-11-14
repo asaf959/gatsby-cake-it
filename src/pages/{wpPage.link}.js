bdc7f5218b55155b662707379aad4b22c67de30f
asif umar <asifkhan03133834@gmail.com>
2022-11-14 12:27:56 +0500

    worked on page side bar

--- src/pages/{wpPage.link}.js
+++ src/pages/{wpPage.link}.js
@@ -1,83 +1,101 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import styled from 'styled-components'
+import React from "react"
+import { graphql } from "gatsby"
+import styled from "styled-components"
 // Components
-import Layout  from '../components/Layout/Layout'
-import PageHero from '../components/PageHero/PageHero'
-import BreadCrumb from '../components/BreadCrumb/BreadCrumb'
+import Layout from "../components/Layout/Layout"
+import PageHero from "../components/PageHero/PageHero"
+import BreadCrumb from "../components/BreadCrumb/BreadCrumb"
+import PageSidebar from "../components/PageSidebar/PageSidebar"
 
 const Wrapper = styled.div`
-max-width: 1180px;
-margin: 0 auto;
-padding: 20px;
+  max-width: 1180px;
+  margin: 0 auto;
+  padding: 20px;
 `
+
 const ContentWrapper = styled.div`
-display: block;
-@media (min-width: 992px) {
-  display: flex;
-}
+  display: block;
+
+  @media (min-width: 992px) {
+    display: flex;
+  }
 `
 
 const PageContent = styled.article`
-margin-top: 20px 0 0 0;
+  margin-top: 20px;
 `
 
-const PageTemplate = ({data}) => (
+const PageTemplate = ({ data }) => (
   <Layout>
     {data.wpPage.featuredImage ? (
-      <PageHero 
-      img={
-        data.wpPage.featuredImage.node.localFile.childImageSharp.gatsbyImageData
-      }
+      <PageHero
+        img={
+          data.wpPage.featuredImage.node.localFile.childImageSharp
+            .gatsbyImageData
+        }
       />
     ) : null}
     <Wrapper>
       <BreadCrumb parent={data.wpPage.wpParent && data.wpPage.wpParent.node} />
-      <p>Side bar</p>
-      <p>Content</p>
-  </Wrapper>
+      <ContentWrapper>
+        <PageSidebar
+          parentChildren={
+            data.wpPage.wpParent && data.wpPage.wpParent.node.wpChildren.nodes
+          }
+          currentPage={data.wpPage}
+          parent={data.wpPage.wpParent && data.wpPage.wpParent.node.title}
+        >
+          {data.wpPage.wpChildren}
+        </PageSidebar>
+        <PageContent>
+          <h1 dangerouslySetInnerHTML={{ __html: data.wpPage.title }} />
+          <div dangerouslySetInnerHTML={{ __html: data.wpPage.content }} />
+        </PageContent>
+      </ContentWrapper>
+    </Wrapper>
   </Layout>
 )
 
 export default PageTemplate
 
 export const pageQuery = graphql`
-query($id: String! ) {
-  wpPage(id: {eq: $id }) {
-    id
-    title
-    content
-    status
-    featuredImage {
-      node {
-        id
-        localFile {
-          childImageSharp {
-            gatsbyImageData(width: 1920, layout: FULL_WIDTH, placeholder: TRACED_SVG)
+  query($id: String!) {
+    wpPage(id: { eq: $id }) {
+      id
+      title
+      content
+      status
+      featuredImage {
+        node {
+          id
+          localFile {
+            childImageSharp {
+              gatsbyImageData(width: 1920, placeholder: TRACED_SVG)
+            }
           }
         }
       }
-    }
-    wpChildren {
-      nodes {
-        ... on WpPage {
-          id
-          uri
-          title
-          wpParent {
-            node {
-              ... on WpPage {
-                id
-                uri
-                title
-                wpChildren {
-                  nodes {
-                    ... on WpPage {
-                      id
-                      title
-                      uri
-                    }
-                  }
+      wpChildren {
+        nodes {
+          ... on WpPage {
+            id
+            uri
+            title
+          }
+        }
+      }
+      wpParent {
+        node {
+          ... on WpPage {
+            id
+            uri
+            title
+            wpChildren {
+              nodes {
+                ... on WpPage {
+                  id
+                  title
+                  uri
                 }
               }
             }
@@ -86,5 +104,4 @@ query($id: String! ) {
       }
     }
   }
-}
 `
